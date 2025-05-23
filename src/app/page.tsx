a963e87016ddc0eb1c90a50474349b26479f6ca7
Kuldeep <kuldeep363rawat@gmail.com>
2025-05-23 16:52:01 +0530

    update: mobile view added for top section and navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="min-h-screen max-w-screen w-full">
       <div
         id="hero-section"
-        className="w-full min-h-screen"
+        className="w-full min-h-screen px-6 relative"
         style={{
           background:
             "linear-gradient(0deg, rgba(231, 229, 255, 1) 0%, rgba(245, 245, 255, 1) 64%)",
@@ -16,21 +16,21 @@ export default function Home() {
       >
         <Navbar />
         <div className="flex justify-center">
-          <div className="mt-36 max-w-[90%] sm:max-w-[40%] flex flex-col items-center">
-            <p className="font-host text-[48px] font-medium text-center leading-none">
+          <div className="mt-36 flex flex-col items-center container">
+            <p className="font-host text-5xl font-medium text-center leading-none">
               Centralised Management
             </p>
-            <p className="font-host text-[32px] font-light text-center">
+            <p className="font-host text-xl sm:text-3xl font-normal text-center">
               for All Your Financial Accounts
             </p>
-            <p className="font-redhat text-[18px] font-light text-center w-[75%]">
-              Effortlessly manage finances with our platform, uniting all bank
-              accounts and actions in one place.
+            <p className="font-redhat text-sm sm:text-lg font-light text-center sm:w-[75%]">
+              Effortlessly manage finances with our platform, <br />
+              uniting all bank accounts and actions in one place.
             </p>
             <Button
               iconPosition="right"
               icon={<ExportCircle color="hsl(var(--white))" size={16} />}
-              className="font-redhat mt-3 w-[90%]"
+              className="font-redhat mt-3 w-full max-w-[500px]"
             >
               Join waitlist
             </Button>
@@ -43,6 +43,15 @@ export default function Home() {
               alt="finioai dashboard"
               width={2000}
               height={1000}
+              className="w-full h-auto"
+            />
+          </div>
+          <div className="block sm:hidden w-[90%] mt-20">
+            <Image
+              src={"https://kuldeep363.github.io/assets/hero-img-sm.png"}
+              alt="finioai dashboard"
+              width={1000}
+              height={2000}
               className="w-full h-auto"
             />
           </div>
--- src/ui/Home/Navbar.tsx
+++ src/ui/Home/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/button/Button";
 import { ExportCircle } from "iconsax-react";
 import Image from "next/image";
+import Menubar from "./Menubar";
 
 const NAV_ITEMS = [
   {
@@ -23,7 +24,7 @@ const NAV_ITEMS = [
 
 const Navbar = () => {
   return (
-    <nav className="w-full py-3 px-6 flex justify-center">
+    <nav className="w-full py-3 flex justify-center">
       <div className=" container">
         <div className="flex justify-between items-center">
           <div className="flex gap-1 sm:gap-2 items-center">
@@ -32,28 +33,34 @@ const Navbar = () => {
               width={15}
               height={30}
               alt="Finioai logo"
-              className="w-[15px] sm:w-[20px]"
+              className="w-[12px] sm:w-[20px]"
             />
             <p className="text-lg sm:text-2xl font-normal text-black">
               FinioAI
             </p>
           </div>
-          <div className="flex gap-6">
+          <div className="hidden sm:flex gap-6">
             {NAV_ITEMS.map((item: any) => {
               return (
-                <p key={item.link} className="font-light cursor-pointer relative inline-block after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:transition-all after:duration-300 hover:after:w-full after:rounded-full after:bg-primary">
+                <p
+                  key={item.link}
+                  className="font-light cursor-pointer relative inline-block after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:transition-all after:duration-300 hover:after:w-full after:rounded-full after:bg-primary"
+                >
                   {item.title}
                 </p>
               );
             })}
           </div>
-          <Button
-            iconPosition="right"
-            icon={<ExportCircle color="hsl(var(--white))" size={16} />}
-            className="font-redhat"
-          >
-            Join waitlist
-          </Button>
+          <div className="hidden sm:block">
+            <Button
+              iconPosition="right"
+              icon={<ExportCircle color="hsl(var(--white))" size={16} />}
+              className="font-redhat"
+            >
+              Join waitlist
+            </Button>
+          </div>
+          <Menubar NAV_ITEMS={NAV_ITEMS} />
         </div>
       </div>
     </nav>
